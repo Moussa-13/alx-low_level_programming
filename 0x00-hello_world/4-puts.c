c52b3cb28c687fa680c76a8b170445a4664d10e2
Moussa-13 <moussakalamamzat@gmail.com>
2022-04-10 21:25:41 +0200

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
-int main(void) {puts("Programming is like building a multilingual puzzle\n");return (0);}
+
+/**
+ * main - prints the phase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
