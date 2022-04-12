c9ae4c76c56c58665ff40a0ffad0de141dea1dc0
Moussa-13 <moussakalamamzat@gmail.com>
2022-04-12 19:46:00 +0200

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always O (Success/correct)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{ 
+		putchar(alphabet);
+	}
+
+	putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0)
+
+	}
+
