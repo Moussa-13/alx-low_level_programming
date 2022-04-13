4dd0fa8169aaa8d7aa07bc4c103c2c5d8d6cd100
Moussa-13 <moussakalamamzat@gmail.com>
2022-04-13 20:15:52 +0200

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 
 int main(void)
 {
-	putchar("_");
-	putchar("p");
-	putchar("u");
-	putchar("t");
-	putchar("c");
-	putchar("h");
-	putchar("a");
-	putchar("r");
-	return 0;
+
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	
+	return (0);
 }
 	
